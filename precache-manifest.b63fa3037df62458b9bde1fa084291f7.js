7a9376fb5777b250a9e940f0844eb093e9c5bbe1
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-03-08 17:29:02 +0100

    Updates

---
+++ precache-manifest.b63fa3037df62458b9bde1fa084291f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2a712a3a03dbb8d2e9590003435d60",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "fdec61a32cc9866efed2",
+    "url": "/birthday-reminder/static/css/main.59fdf0a5.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdec61a32cc9866efed2",
+    "url": "/birthday-reminder/static/js/main.f0df7819.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
